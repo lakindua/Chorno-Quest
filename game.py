4bae5d2f2cb1730d72affdddb7bdb5fa035dd9e2
lakindua <lakindua@metropolia.fi>
2025-10-01 15:42:43 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,28 @@
-#database connection
+#import
+import random
+import pyfiglet
+import mysql.connector
+from geopy import distance
+
+
+# Database connection
+conn = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='test1',
+    user='root',
+    password='12345',
+    autocommit=True
+)
+
 #eras(energy consumption)
+ERAS = {
+    'ANCIENT': 1.2,
+    'MEDIEVAL': 1.1,
+    'MODERN': 1.0,
+    'FUTURE': 0.9
+}
+
 #get 15 random airports(function)
 #get all goals(function)
 #create a new game(function)
